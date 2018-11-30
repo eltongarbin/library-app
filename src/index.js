b504b0d6b107032a17874fe7b452c85264ba7f26
Elton Garbin <eltongarbin@gmail.com>
2018-11-29 22:23:39 -0200

    install and configure the entry pont of react-intl

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+import fr from 'react-intl/locale-data/fr';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import messages from './messages';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+addLocaleData([...en, ...es, ...fr]);
+
+const locale =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage ||
+  'en-US';
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages[locale]}>
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
